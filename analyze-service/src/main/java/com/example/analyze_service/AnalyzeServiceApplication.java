9ff0287d708f04eb9309140ef268c37f66a9acb3
NipunaLankara <nipunalankara@gmail.com>
2026-01-13 14:15:06 +0530

    task analyze, create ai-service and analyze-service

---
+++ analyze-service/src/main/java/com/example/analyze_service/AnalyzeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.analyze_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzeServiceApplication.class, args);
+	}
+
+}
